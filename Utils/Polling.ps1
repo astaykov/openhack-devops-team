1578f7cdfafb71bd9977a3892bce76435b8a00f6
Anton Staykov <astaykov@gmail.com>
2020-09-23 14:01:51 +0200

    inlcuing polling script

---
+++ Utils/Polling.ps1
@@ -0,0 +1,60 @@
+<#
+
+.SYNOPSIS
+    Healthcheck the Uri and display the result.
+
+.DESCRIPTION
+    The polling.ps1 display the helthcheck result for each second. You will see the format
+    [Timestamp] | [StatusCode] | [Uri]
+
+.PARAMETER Uri
+    The target uri for checking the status
+
+.PARAMETER displayUri
+    If $true, it displays the Uri in output
+
+.EXAMPLE
+PS > .\polling.ps1 -Uri https://github.com/Azure-Samples/openhack-devops-proctor -displayUri $true
+.\polling.ps1 -Uri https://github.com/Azure-Samples/openhack-devops-proctor -displayUri $true
+21/03/2019 14:21:17 | 200 | https://github.com/Azure-Samples/openhack-devops-proctor
+21/03/2019 14:21:21 | 200 | https://github.com/Azure-Samples/openhack-devops-proctor
+
+.EXAMPLE
+PS > .\polling.ps1 -Uri https://github.com/Azure-Samples/openhack-devops-proctor
+21/03/2019 14:21:55 | 200
+21/03/2019 14:21:58 | 200
+
+#>
+
+Param(
+    [string] [Parameter(Mandatory=$true)] $Uri,
+    [boolean] [Parameter(Mandatory=$false)] $displayUri,
+    [int] [Parameter(Mandatory=$false)] $timeoutInSeconds = 60
+    )
+
+$timer = 0
+
+while($true) {
+  $R = Invoke-WebRequest -URI $Uri
+  $timestamp = Get-Date
+  $output = ""
+  if ($displayUri) {
+    $output = '{0} | {1} | {2} | {3}' -f($timestamp, $R.StatusCode, $Uri, $R.Content)
+    
+  } else {
+
+    $output = '{0} | {1}' -f($timestamp, $R.StatusCode)
+  }
+  Write-Output $output
+  $statusJson = $R.Content | ConvertFrom-Json
+  if($statusJson.status -eq "healthy")
+  {
+    exit 0
+  }
+  Start-Sleep -Seconds 1
+  $timer = $timer + 1
+  if($timer -ge $timeoutInSeconds)
+  {
+    exit 1
+  }
+}
