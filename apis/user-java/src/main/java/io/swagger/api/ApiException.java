6c2a7cc368dbc0e746a33b4d5fdcf804040492db
Anton Staykov <astaykov@gmail.com>
2020-09-23 10:18:56 +0200

    this change will break tests.

--- apis/user-java/src/main/java/io/swagger/api/ApiException.java
+++ apis/user-java/src/main/java/io/swagger/api/ApiException.java
@@ -6,6 +6,6 @@ public class ApiException extends Exception{
     private int code;
     public ApiException (int code, String msg) {
         super(msg);
-        this.code = code + 'somethin';
+        this.code = code;
     }
 }
