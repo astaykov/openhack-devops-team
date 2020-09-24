76a0d38ad30dbccacfe59eb011aae6acbf7c4d64
Anton Staykov <astaykov@gmail.com>
2020-09-24 14:02:24 +0200

    buah

--- apis/trips/tripsgo/healthCheckService.go
+++ apis/trips/tripsgo/healthCheckService.go
@@ -9,11 +9,7 @@ func healthcheckGet(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
 
-<<<<<<< HEAD
-	hc := &Healthcheck{Message: "Trip Service Healthcheck", Status: "NOT SO Healthy"}
-=======
-	hc := &Healthcheck{Message: "Trip Service Healthcheck", Status: "not Healthy"}
->>>>>>> d8b7f5866ba704a631b3c4ba4f0da81c44eff946
+	hc := &Healthcheck{Message: "Trip Service Healthcheck", Status: "not so Healthy"}
 
 	json.NewEncoder(w).Encode(hc)
 }
